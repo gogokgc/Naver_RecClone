023883f208a202a70803ade333bbd14ed38250ca
gogokgc <gogokgc@gmail.com>
2024-04-16 21:05:29 +0900

    Initial Commit

---
+++ Naver_RecClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Naver_RecClone
+//
+//  Created by KYUCHEOL KIM on 4/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Naver_RecClone/Naver_RecCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Naver_RecCloneApp.swift
+//  Naver_RecClone
+//
+//  Created by KYUCHEOL KIM on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Naver_RecCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
