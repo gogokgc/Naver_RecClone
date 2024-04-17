e372aeb3fa39b125ad8e199a6392241fa2aefee4
gogokgc <gogokgc@gmail.com>
2024-04-17 21:19:50 +0900

    온보딩 화면 화면이동 기초공사

--- Naver_RecClone/Feature/Onboarding/OnboardingView.swift
+++ Naver_RecClone/Feature/Onboarding/OnboardingView.swift
@@ -6,11 +6,32 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    @StateObject private var pathModel = PathModel()
     @StateObject private var onboardingViewModel = OnboardingViewModel()
     
     var body: some View {
-        // TODO: - 화면전환 구현 필요
-        OnboardingContentView(onboardingViewModel: onboardingViewModel)
+        NavigationStack(path: $pathModel.paths) {
+            OnboardingContentView(onboardingViewModel: onboardingViewModel)
+                .navigationDestination(
+                    for: PathType.self,
+                    destination: {pathType in
+                        switch pathType {
+                        case .homeView:
+                            HomeView()
+                                .navigationBarBackButtonHidden()
+                            
+                        case .todoView:
+                            TodoView()
+                                .navigationBarBackButtonHidden()
+                            
+                        case .memoView:
+                            MemoView()
+                                .navigationBarBackButtonHidden()
+                        }
+                    }
+                )
+        }
+        .environmentObject(pathModel)
     }
       
   
@@ -109,10 +130,11 @@ private struct OnboardingCellView: View {
 
 // MARK: - 시작하기 버튼 뷰
 private struct StartBtnView: View {
+    @EnvironmentObject private var pathModel: PathModel
     
     var body: some View {
         Button(
-            action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/,
+            action: {pathModel.paths.append(.homeView)},
             label: {
                 HStack{
                     Text("시작하기")
