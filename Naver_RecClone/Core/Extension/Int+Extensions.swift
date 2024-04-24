41f164ad898d0b76ff65032d09d8a2a5466ad2f0
gogokgc <gogokgc@gmail.com>
2024-04-24 21:03:03 +0900

    타이머용 익스텐션 생성

--- Naver_RecClone/Core/Extension/Int+Extensions.swift
+++ Naver_RecClone/Core/Extension/Int+Extensions.swift
@@ -6,5 +6,26 @@
 import Foundation
 
 extension Int {
-  
+    var formattedTimeString: String {
+        let time = Time.fromSeconds(self)
+        let hoursString = String(format: "0%2d", time.hours)
+        let minutesString = String(format: "0%2d", time.minutes)
+        let secondsString = String(format: "0%2d", time.seconds)
+        
+        return "\(hoursString) : \(minutesString) : \(secondsString)"
+    }
+    
+    var formattedSettingTime: String {
+        let currentDate = Date()
+        let settingDate = currentDate.addingTimeInterval(TimeInterval(self))
+        
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_KR")
+        formatter.timeZone = TimeZone(identifier: "Asia/Seoul")
+        formatter.dateFormat = "HH:mm"
+        
+        let formattedTime = formatter.string(from: settingDate)
+        
+        return formattedTime
+    }
 }
